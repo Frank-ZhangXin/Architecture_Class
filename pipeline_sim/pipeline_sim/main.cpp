055aa1cf6d1cc3b58d532a8b00f2c16444e555bc
Xin Zhang <zxuuzx@gmail.com>
2013-10-06 00:59:33 -0500

    First commit :D

---
+++ pipeline_sim/pipeline_sim/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  pipeline_sim
+//
+//  Created by Veroinica Kirin on 10/6/13.
+//  Copyright (c) 2013 Xin Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
