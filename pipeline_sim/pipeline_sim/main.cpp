3ec661f36c9d6c3d09dd8e80fea19959e677219b
Xin Zhang <zxuuzx@gmail.com>
2013-10-06 15:38:42 -0500

    Developed pipeline prototype established. 3:38 Oct 6

--- pipeline_sim/pipeline_sim/main.cpp
+++ pipeline_sim/pipeline_sim/main.cpp
@@ -10,55 +10,58 @@
 #include <thread>
 #include <string>
 #include <mutex>
+#include <queue>
+#include <stack>
+
 
 using namespace std;
 
-mutex t_lock;
-
-
-
-string stg_IF(){
-    //lock_guard<mutex> lk(t_lock);
-    string str = "This is IF stage.";
-    return str;
-}
-
-string stg_ID(){
-    //lock_guard<mutex> lk(t_lock);
-    string str = "This is ID stage.";
-    return str;
-}
-
-void inst(){
-    lock_guard<mutex> lk(t_lock);
-    cout << stg_IF() << endl;
-    cout << stg_ID() << endl;
+struct bank_account
+{
+    explicit bank_account(string name, string title)
+    {
+        sName = name;
+        mTitle = title;
+    }
     
-}
+    string sName;
+    string mTitle;
+    mutex mMutex;
+};
 
+void transfer( bank_account &from, bank_account &to)
+{
+    // don't actually take the locks yet
+    unique_lock<mutex> lock1( from.mMutex, defer_lock );
+    unique_lock<mutex> lock2( to.mMutex, defer_lock );
+    
+    // lock both unique_locks without deadlock
+    lock( lock1, lock2 );
+    
+    string title = from.mTitle;
+    from.mTitle = to.mTitle;
+    to.mTitle = title;
+    
+    // output log
+    cout << from.sName << "'s stage is " << from.mTitle << endl;
+    cout << to.sName << "'s stage is " << to.mTitle << endl;
+    cout << "\n";
+}
 
 int main(int argc, const char * argv[])
 {
-    //thread thrd_1(inst);
-    //thread thrd_2(inst);
-    //thread thrd_3(inst);
-    //thread thrd_4(inst);
-
+    bank_account Instruction1( "Instruction_1", "ID" );
+    bank_account Instruction2( "Instruction_2", "IF" );
     
-    //thrd_1.join();
-    //thrd_2.join();
-    //thrd_3.join();
-    //thrd_4.join();
+    cout << Instruction1.sName << "'s stage is " << Instruction1.mTitle << endl;
+    cout << Instruction2.sName << "'s stage is " << Instruction2.mTitle << endl;
+    cout << "\n";
     
-    int i = 0;
-    while (true) {
-        thread n(inst);
-        n.join();
-        i++;
-        if (i == 10) {
-            break;
-        }
-    }
+    thread t1( [&](){ transfer( Instruction1, Instruction2); transfer( Instruction1, Instruction2); } );
+    //thread t2( [&](){ transfer( Account1, Account2); } );
+    
+    t1.join();
+    //t2.join();
     
     return 0;
     
