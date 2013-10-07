2a163257fa6b6437ab06ef952226603cb9fb682f
Xin Zhang <zxuuzx@gmail.com>
2013-10-06 22:30:22 -0500

    thread pipeline final progress. T^T

--- pipeline_sim/pipeline_sim/main.cpp
+++ pipeline_sim/pipeline_sim/main.cpp
@@ -6,11 +6,14 @@
 //  Copyright (c) 2013 Xin Zhang. All rights reserved.
 //
 
+
+
 #include <iostream>
 #include <thread>
 #include <string>
 #include <mutex>
 #include <stack>
+#include <chrono>
 
 
 using namespace std;
@@ -31,16 +34,16 @@ struct inst
 stack<string> stg_stk;
 
 void stg_init(stack<string> *stg_stk){
+    stg_stk->push("$");
+    stg_stk->push("$");
+    stg_stk->push("$");
+    stg_stk->push("$");
     stg_stk->push("IF");
-    stg_stk->push("ID");
-    stg_stk->push("EX");
-    stg_stk->push("MEM");
-    stg_stk->push("WB");
 }
 
 
-void transfer( inst &i1, inst &i2, inst &i3, inst &i4, inst &i5)
-{
+void transfer( inst &i1, inst &i2, inst &i3, inst &i4, inst &i5){
+    
     // don't actually take the locks yet
     unique_lock<mutex> lock1( i1.mMutex, defer_lock );
     unique_lock<mutex> lock2( i2.mMutex, defer_lock );
@@ -52,16 +55,60 @@ void transfer( inst &i1, inst &i2, inst &i3, inst &i4, inst &i5)
     lock( lock1, lock2, lock3, lock4, lock5 );
     
     stack<string> *ptr = &stg_stk;
-    i1.iStage = ptr->top();
-    ptr->pop();
-    i2.iStage = ptr->top();
-    ptr->pop();
-    i3.iStage = ptr->top();
-    ptr->pop();
-    i4.iStage = ptr->top();
-    ptr->pop();
-    i5.iStage = ptr->top();
-    ptr->pop();
+    
+    if (ptr->top() != "$") {
+        i1.iStage = ptr->top();
+        ptr->pop();
+        if (ptr->top() != "$") {
+            i2.iStage = ptr->top();
+            ptr->pop();
+            if (ptr->top() != "$") {
+                i3.iStage = ptr->top();
+                ptr->pop();
+                if (ptr->top() != "$") {
+                    i4.iStage = ptr->top();
+                    ptr->pop();
+                    if (ptr->top() != "$") {
+                        i5.iStage = ptr->top();
+                        ptr->pop();
+                        ptr->push("IF");
+                        ptr->push("ID");
+                        ptr->push("EX");
+                        ptr->push("MEM");
+                        ptr->push("WB");
+                        
+                        // from here we should increase instruction queue index.
+                        
+                    }
+                    else{
+                        ptr->push("IF");
+                        ptr->push("ID");
+                        ptr->push("EX");
+                        ptr->push("MEM");
+                        ptr->push("WB");
+                    }
+                }
+                else{
+                    ptr->push("IF");
+                    ptr->push("ID");
+                    ptr->push("EX");
+                    ptr->push("MEM");
+                }
+            }
+            else{
+                ptr->push("IF");
+                ptr->push("ID");
+                ptr->push("EX");
+            }
+        }
+        else{
+            ptr->push("IF");
+            ptr->push("ID");
+        }
+        
+    }
+    else ptr->push("IF");;
+
     
     // output log
     cout << i1.iName << "'s stage is " << i1.iStage << endl;
@@ -70,7 +117,10 @@ void transfer( inst &i1, inst &i2, inst &i3, inst &i4, inst &i5)
     cout << i4.iName << "'s stage is " << i4.iStage << endl;
     cout << i5.iName << "'s stage is " << i5.iStage << endl;
     cout << "\n";
+    
+    i1.iStage = "$";
 }
+
 
 int main(int argc, const char * argv[])
 {
@@ -79,16 +129,45 @@ int main(int argc, const char * argv[])
     inst Instruction3( "Instruction_3", "$" );
     inst Instruction4( "Instruction_4", "$" );
     inst Instruction5( "Instruction_5", "$" );
+    inst Instruction6( "Instruction_6", "$" );
+    inst Instruction7( "Instruction_6", "$" );
+    
     
     // Initialize stack that is storing stages.
     stg_init(&stg_stk);
     
     
     thread t1( [&](){ transfer( Instruction1, Instruction2, Instruction3, Instruction4, Instruction5 ); } );
-    //thread t2( [&](){ transfer( Instruction1, Instruction2); } );
+    thread t2( [&](){ transfer( Instruction1, Instruction2, Instruction3, Instruction4, Instruction5 ); } );
+    thread t3( [&](){ transfer( Instruction1, Instruction2, Instruction3, Instruction4, Instruction5 ); } );
+    
+    /*
+    thread t4( [&](){ transfer( Instruction1, Instruction2, Instruction3, Instruction4, Instruction5 ); } );
+    thread t5( [&](){ transfer( Instruction1, Instruction2, Instruction3, Instruction4, Instruction5 ); } );
+    thread t6( [&](){ transfer( Instruction2, Instruction3, Instruction4, Instruction5, Instruction6 ); } );
+    thread t7( [&](){ transfer( Instruction2, Instruction3, Instruction4, Instruction5, Instruction6 ); } );
+    thread t8( [&](){ transfer( Instruction2, Instruction3, Instruction4, Instruction5, Instruction6 ); } );
+    thread t9( [&](){ transfer( Instruction2, Instruction3, Instruction4, Instruction5, Instruction6 ); } );
+    thread t10( [&](){ transfer( Instruction2, Instruction3, Instruction4, Instruction5, Instruction6 ); } );
+    thread t11( [&](){ transfer( Instruction3, Instruction4, Instruction5, Instruction6, Instruction7 ); } );
+    thread t12( [&](){ transfer( Instruction3, Instruction4, Instruction5, Instruction6, Instruction7 ); } );
+    */
+    
     
     t1.join();
-    //t2.join();
+    t2.join();
+    t3.join();
+    /*
+    t4.join();
+    t5.join();
+    t6.join();
+    t7.join();
+    t8.join();
+    t9.join();
+    t10.join();
+    t11.join();
+    t12.join();
+    */
     
     return 0;
     
